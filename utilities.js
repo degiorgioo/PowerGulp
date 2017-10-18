92071af30de7478033182edca5bb30a27bd2ca29
Davide De Giorgio <davide.degiorgio@namics.com>
2017-10-18 16:11:05 +0200

    getTask-Function changed

--- utilities.js
+++ utilities.js
@@ -2,12 +2,8 @@
 var gulp = require('gulp');
 var gulpconfig = require('./gulp-config');
 
-function getTask(taskname, gulpDeps){
-	if(gulpDeps){
-		return require(gulpconfig.taskspath + taskname)(gulp, gulpconfig, gulpDeps);
-	}else{
-		return require(gulpconfig.taskspath + taskname)(gulp, gulpconfig);
-	}
+function getTask(taskname, gulpDeps) {
+	return require(gulpconfig.taskspath + taskname)(gulp, gulpconfig, gulpDeps);
 }
 
 module.exports = {
