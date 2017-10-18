dac22355f521100adb196a73d7b6a70fe0b3d66f
Davide De Giorgio <davide.degiorgio@namics.com>
2017-10-18 07:35:30 +0200

    sass-task and folder/files for this created

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 'use strict';
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('default', function() {
 	// place code for your default task here
 });
 
+gulp.task('sass', function () {
+	return gulp.src('./sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('./css'));
+});
