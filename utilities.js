1ec27ec7a0629b30cb68ae5157107f7225e53f10
Davide De Giorgio <davide.degiorgio@namics.com>
2017-10-18 16:15:23 +0200

    jsDoc of method 'getTask' created

--- utilities.js
+++ utilities.js
@@ -2,6 +2,14 @@
 var gulp = require('gulp');
 var gulpconfig = require('./gulp-config');
 
+/**
+ * This function searches in the gulpconfig.taskpath-folder a '.js'-file named as the param
+ * given on function-call.
+ *
+ * @param taskname
+ * @param gulpDeps
+ * @returns A Method-Object with the params: gulp, gulpconfig and gulpDeps injected.
+ */
 function getTask(taskname, gulpDeps) {
 	return require(gulpconfig.taskspath + taskname)(gulp, gulpconfig, gulpDeps);
 }
