8c36a1168401832d4518197e7b90099cfcacf4ae
Davide De Giorgio <davide.degiorgio@namics.com>
2017-10-19 13:42:36 +0200

    concat-file to 'concat-js.js' renamed

--- gulpfile.js
+++ gulpfile.js
@@ -6,4 +6,4 @@ var getTask = require('./utilities').getTask;
 gulp.task('default', getTask('default'));
 gulp.task('sass', getTask('sass'));
 gulp.task('watch-sass', getTask('watch-sass', ['sass']));
-gulp.task('concat-js', getTask('concat'));
+gulp.task('concat-js', getTask('concat-js'));
